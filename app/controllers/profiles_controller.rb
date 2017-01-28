ba6c4e68a9a370ac27b7ba83fbb2d2aa58fb848b
Saul Ocampo <digitalh2o@gmail.com>
2017-01-28 18:54:57 +0000

    Added user profiiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@ class ProfilesController < ApplicationController
   
   # GET to /users/:user_id/profile/new
   def new
-    # render blank profile details form
+    @profile = Profile.new
   end
 end
