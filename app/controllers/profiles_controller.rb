b364230adabe3e188fc5170e04fefcb3fbf56ffa
Saul Ocampo <digitalh2o@gmail.com>
2017-01-29 03:29:47 +0000

    Added file field for image upload to profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -12,7 +12,7 @@ class ProfilesController < ApplicationController
     # Create profile linked to this specific user
     @profile = @user.build_profile(profile_params)
     if @profile.save
-      flash[:succes] = "Profile updated!"
+      flash[:success] = "Profile updated!"
         redirect_to user_path(params[:user_id])
     else
       render :new
@@ -21,6 +21,6 @@ class ProfilesController < ApplicationController
   
   private
     def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
 end
